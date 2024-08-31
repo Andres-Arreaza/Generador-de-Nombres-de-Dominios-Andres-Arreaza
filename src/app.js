c4d58218a8877537cf6ec093a0de57e31dc82008
Andrés Arreaza <112995166+Andres-Arreaza@users.noreply.github.com>
2024-08-31 05:32:48 +0000

    finalizacion de proyecto

--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function generadorDominios() {
+    let pronoun = ["the", "our"];
+    let adj = ["great", "big"];
+    let noun = ["jogger", "racoon"];
+
+    //Declaramos un array donde se guardaran todos los strings.
+    const posibilidades = [];
+
+    //Creamos un loop for pata iterar los items del array.
+    for (let i = 0; i <= pronoun.length - 1; i++) {
+      for (let j = 0; j <= adj.length - 1; j++) {
+        for (let k = 0; k <= noun.length - 1; k++) {
+          posibilidades.push(`${pronoun[i]}${adj[j]}${noun[k]}.com`);
+        }
+      }
+    }
+    return posibilidades;
+  }
+
+  //Creamos una funcion que nos imprima todos las posibilidades de dominios que hay.
+  function imprimirDominios() {
+    document.getElementById("posibilidad").innerHTML = generadorDominios().join(
+      "<br>"
+    );
+  }
+  imprimirDominios();
 };
